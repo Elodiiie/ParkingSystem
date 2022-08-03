0d6366bf4b36c4f401a898769cc8581190d385f5
Elodiiie <glx983793727@163.com>
2022-08-03 21:54:09 +0800

    Migrated from the old repo

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableCaching
 @EnableEncryptableProperties
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
